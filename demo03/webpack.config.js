c0a63898f72aada5d6390c260aeef55fe8407263
lyctea <396136337@qq.com>
2016-11-29 11:10:13 +0800

    demo03

---
+++ demo03/webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: './main.jsx',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+loaders: [
+  {
+    test: /\.js$/,
+    exclude: /(node_modules|bower_components)/,
+    loader: 'babel-loader',
+    query: {
+      presets: ['es2015']
+    }
+  }
+]
+}
+};
